a05cef2e17328fb3924e02d64f64a1750bab47df
Matthew Campbell <cattmampbell@gmail.com>
2018-11-18 17:17:26 -0500

    Connected index.html and scripts.js files via script tag.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log(`Testing... Testing... 1, 2, 3, 4, Dicey Business.`);
+
+/* More on Colorful Console Messages here: https://www.samanthaming.com/tidbits/40-colorful-console-message */
+// console.log('%cStart%c of scripts.js file!', 'font-weight: 900; color: blue;', 'font-weight: 400; color: blue;'); // Should log: Start of scripts.js file!
+// console.log(`%c. . .`, 'color: grey; font-weight: 800; font-size: 12px;');
+
+// console.log(`%c. . .`, 'color: grey; font-weight: 800; font-size: 12px;');
+// console.log('%cEnd%c of scripts.js file!', 'font-weight: 900; color: blue;', 'font-weight: 400; color: blue;'); // Should log: End of scripts.js file!
